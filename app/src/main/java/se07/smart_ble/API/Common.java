5f1c79b734e86daec70fa647d140a30db76ceae1
Minh Quan <minh.quan@singalarity.com>
2016-12-14 22:51:12 +0700

    edit flow

--- app/src/main/java/se07/smart_ble/API/Common.java
+++ app/src/main/java/se07/smart_ble/API/Common.java
@@ -6,7 +6,7 @@ package se07.smart_ble.API;
 
 public class Common {
     //static final String SERVICE_API_URL = "http://192.168.137.1:8081/register";
-    public static final String SERVICE_API_URL = "http://10.0.2.2:8081";
+    public static final String SERVICE_API_URL = "http://10.10.10.10:8081";
     public static final int RESULT_SUCCESS = 0;
     public static final int RESULT_ERROR= 1;
     public static final int RESULT_USER_EXISTS = 2;
--- app/src/main/java/se07/smart_ble/LockData.java
+++ app/src/main/java/se07/smart_ble/LockData.java
@@ -7,15 +7,12 @@ public class LockData {
 
     private String mName;
     private String mMAC;
-    private String mSK;
 
-    public LockData(String _name, String _mac, String _sk){
+    public LockData(String _name, String _mac){
         this.mName = _name;
         this.mMAC = _mac;
-        this.mSK = _sk;
     }
 
     public String get_mName(){return mName;}
     public String get_mMAC(){return mMAC;}
-    public String get_mSk(){return mSK;}
 }
